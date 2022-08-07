1f2aec6cd2bc96b365ff8748000d991cdbf060dd
Hannes Winkler <hanneswinkler2000@web.de>
2022-08-07 21:53:05 +0200

    remove libc frontend builder stubs

--- packages/_ardera_libc_bindings_generator/lib/builder.dart
+++ packages/_ardera_libc_bindings_generator/lib/builder.dart
@@ -4,15 +4,7 @@ import 'package:source_gen/source_gen.dart';
 
 import 'src/libc_bindings_generator.dart';
 
-Builder libcBindingsBuilder(BuilderOptions options) {
-  return LibraryBuilder(LibCBindingsGenerator(
-    options: options.config,
-    logger: Logger.root,
-  ));
-}
-
 Builder libcPlatformBackendBuilder(BuilderOptions options) {
-  print("constructing builder");
   return LibraryBuilder(
     LibCPlatformBackendGenerator(
       options: options.config,
