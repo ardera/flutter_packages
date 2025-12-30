407a39d332c8bf14e3aec332a47d42925e44dee8
Igor Demyanov <igor.demyanov@gmail.com>
2025-12-30 23:38:22 +0300

    fix dpms import

--- packages/dpms/lib/dpms.dart
+++ packages/dpms/lib/dpms.dart
@@ -1,2 +1,2 @@
-export 'src/dpms_stub.dart' if (dart.library.io) 'src/linux_dpms.dart' if (dart.library.js) 'src/linux_dpms_web.dart';
+export 'src/dpms_stub.dart' if (dart.library.io) 'src/dpms.dart' if (dart.library.js) 'src/dpms_web.dart';
 export 'src/power_mode.dart';
